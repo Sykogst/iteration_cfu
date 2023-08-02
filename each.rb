5e1f1a6bedeb096574c0b0a6c48c141db1e28dbd
Sam Tran <samuelttran@gmail.com>
2023-08-02 11:09:40 -0600

    Complete challenge 2

--- each.rb
+++ each.rb
@@ -13,5 +13,13 @@ end
 # Use the each method to print out the words that are exactly 3 characters in length
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 
+# calling each method, and name single element
+words.each do |word|
+# code block conditional to check if length is exactly 3 characters
+  if word.length == 3
+    # print character
+    puts word
+  end
+end
 
 # Record yourself live-coding and talking through either Challenge 1 or 2. The recording doesn't have to be the first time you are solving the problem. Use technical vocabulary and explain each part to demonstrate your understanding! Share the link to your video in your small group channel.
